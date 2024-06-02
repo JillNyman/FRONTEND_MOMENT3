8d283549a802932be5429649285d6582c5eaab59
JillNyman <jill.engvall@gmail.com>
2024-06-02 15:19:52 +0200

    Rättat till
    Fick feedback på uppgift 3 och 4 och därför gjort följande:
    - skapat mörkt tema med switch (uppg 3)
    - längre utvärdering av SASS/SCSS (uppg 3)
    - animering vid användarinteraktion (uppg 4

---
+++ README.md
@@ -0,0 +1,10 @@
+Det här repositoriet motsvarar uppgift 3, 4 och 5 i kursen Frontend-baserad webbutveckling inom Datateknik GR (B) vid Mittuniversitetet.
+
+Uppgift 3: CSS-preprocessorer
+Uppgift 4: Grafisk effekter med CSS
+Uppgift 5: Dynamiska webbplatser
+
+Skapat med node js.
+
+Följande paket används:
+parcel, scss, chart.js, sharp, googlemaps/js-api-loader
--- src/js/theme.js
+++ src/js/theme.js
@@ -1,9 +1,11 @@
 let checkbox = document.querySelector("input[name=theme-switch]");
 
 if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+
     document.documentElement.setAttribute("data-theme", "dark");
     checkbox.checked = true;
 } else {
+    
     document.documentElement.setAttribute("data-theme", "light");
     checkbox.checked = false;
 }
